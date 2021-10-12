f59350a286be652e546eca62a42be4c10086de9d
Emory Ducote <59099798+etd4114@users.noreply.github.com>
2021-10-12 17:04:38 -0400

    Update Display.h

--- Midterm_Keyboard_Demo/Display.h
+++ Midterm_Keyboard_Demo/Display.h
@@ -1,17 +1,32 @@
 #ifndef Display_h
 #define Display_h
 
-#define DEN_HIGH (0x01 |= 34)
-#define DEN_LOW  (0x00 &= 34)
-#define CSX_HIGH (0x01 |= 4)
-#define CSX_LOW  (0x00 &= 4)
+#define REG_HCYCLE      0x2C
+#define REG_HOFFSET     0x30
+#define REG_HSYNC0      0x38
+#define REG_HSYNC1      0x3C
+#define REG_VCYCLE      0x40
+#define REG_VOFFSET     0x44
+#define REG_VSYNC0      0x4C
+#define REG_VSYNC1      0x50
+#define REG_SWIZZLE     0x64
+#define REG_PCLK_POL    0x6C
+#define REG_CSPREAD     0x68
+#define REG_HSIZE       0x34
+#define REG_VSIZE       0x48
+#define REG_DLSWAP      0x54
+#define REG_GPIO_DIR    0x90
+#define REG_GPIO        0x94
+#define REG_PCLK        0x70
 
-//class LCD_Display {
-//    public:
-//            void Write_Data();
-//            void Display_Init();
-//};
+#define RAM_DL          0x100
 
-void Display_Init(void);
-
-#endif /*Display_h*/
+class LCD_Display {
+    public:
+            void Display_Init();
+            void Vertex_Select_Coordinate(int16_t X, int16_t Y);
+            void Vertex_Select_Pixel(int8_t X, int8_t Y, int8_t handle, int8_t cell);
+            void Begin(int8_t prim);
+            void Set_Line_Width(int16_t width);
+            void Send_Text(int16_t X, int16_t y, int16_t font, uint16_t options, const char* s);
+};
