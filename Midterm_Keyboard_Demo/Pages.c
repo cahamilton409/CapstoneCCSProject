31d643a372f04f13d20ac00cc29dfc520a189fee
Emory Ducote <59099798+etd4114@users.noreply.github.com>
2021-10-19 14:08:35 -0400

    Update Pages.c

--- Midterm_Keyboard_Demo/Pages.c
+++ Midterm_Keyboard_Demo/Pages.c
@@ -1,14 +1,14 @@
 #include "driverlib.h"
 
-void Spanish_Home() {
-
+void Spanish_Home(vector<char> characters) {
+  
 }
 
 void Spanish_Pressed(int16_t press_location) {
   
 }
 
-void French_Home() {
+void French_Home(vector<char> characters) {
 
 }
 
