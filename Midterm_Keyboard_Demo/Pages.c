aaed2a158624761c85b068f8f42d3fe536e37938
Emory Ducote <59099798+etd4114@users.noreply.github.com>
2021-10-19 14:29:05 -0400

    Update Pages.c

--- Midterm_Keyboard_Demo/Pages.c
+++ Midterm_Keyboard_Demo/Pages.c
@@ -1,17 +1,30 @@
 #include "driverlib.h"
 
-void Spanish_Home(vector<char> characters) {
-  
+void Spanish_Home(char[] characters) {
+    int16_t[] X_coords;
+    int16_t[] Y_coords;
+    int16_t length;
+    Create_Grid_Given_Points(X_coords, Y_coords, length, characters)
 }
 
 void Spanish_Pressed(int16_t press_location) {
-  
+    int16_t[] X_coords;
+    int16_t[] Y_coords;
+    int16_t length;
+    Create_Grid_Given_Points(X_coords, Y_coords, length, characters)
+    // ADD PRESSED KEY DARKNESS
 }
 
-void French_Home(vector<char> characters) {
-
+void French_Home(char[] characters) {
+    int16_t[] X_coords;
+    int16_t[] Y_coords;
+    int16_t length;
+    Create_Grid_Given_Points(X_coords, Y_coords, length, characters)
 }
 
 void French_Pressed(int16_t press_location) {
-  
+    int16_t[] X_coords;
+    int16_t[] Y_coords;
+    int16_t length;
+    Create_Grid_Given_Points(X_coords, Y_coords, length, characters)
 }
