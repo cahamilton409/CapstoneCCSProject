eeaaa97708b8de7481edddfa936b88c53e2da1c5
Emory Ducote <59099798+etd4114@users.noreply.github.com>
2021-10-19 13:48:16 -0400

    Create Pages.c

---
+++ Midterm_Keyboard_Demo/Pages.c
@@ -0,0 +1,17 @@
+#include "driverlib.h"
+
+void Spanish_Home() {
+
+}
+
+void Spanish_Pressed(int16_t press_location) {
+  
+}
+
+void French_Home() {
+
+}
+
+void French_Pressed(int16_t press_location) {
+  
+}
