e95280b7f3c9d159c9eb5f0fe74bb28169191626
Rohan Chandra <58828165+rohanchandra-cpe@users.noreply.github.com>
2021-10-26 12:18:50 -0400

    Update Audio.c
    Initializing pins

--- Midterm_Keyboard_Demo/Audio.c
+++ Midterm_Keyboard_Demo/Audio.c
@@ -4,8 +4,22 @@
  *  Created on: Sep 28, 2021
  *      Author: Chris
  */
+#include <stdint.h>
+#include "msp.h"
+#include "Clock.h"
+#include "TExaS.h"
+
+volatile uint8_t DataA; // Data from Pin A
+volatile uint8_t DataB; // Data from Pin B
 
 void Audio_Init(void) {
+    P4->SEL0 &= ~0x01;
+    P4->SEL1 &= ~0x01;    //  P4.0 as GPIO
+    P4->DIR &= ~0x01;      //  make P4.0 out
+    
+    P3->SEL0 &= ~0x80;
+    P3->SEL1 &= ~0x80;    //  P4.0 as GPIO
+    P3->DIR &= ~0x80;      //  make P4.0 out
     return;
 }
 
