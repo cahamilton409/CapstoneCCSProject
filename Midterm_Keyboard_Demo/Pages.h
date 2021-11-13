db41256130d8320d8462fd8977140656a5cd25b0
Christopher Hamilton <christopherarthamilton@gmail.com>
2021-11-13 09:06:48 -0500

    Remove Midterm_Keyboard_Demo

--- Midterm_Keyboard_Demo/Pages.h
+++
@@ -1,6 +0,0 @@
-#include "Display.h"
-
-void Spanish_Home();
-void Spanish_Pressed(int16_t location);
-void French_Home();
-void French_Pressed(int16_t location);
--- Midterm_Keyboard_Demo/README.md
+++
@@ -1,2 +0,0 @@
-PIXEL MAP
-![image](https://user-images.githubusercontent.com/59099798/137954203-d38e39aa-562c-496b-819d-b43fb805d5f9.png)
