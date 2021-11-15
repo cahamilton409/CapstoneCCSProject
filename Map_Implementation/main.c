9d5f13d3491f56e2e2dfe96852440064ef0e2fa3
Christopher Hamilton <christopherarthamilton@gmail.com>
2021-11-14 21:26:15 -0500

    Implement key rearranging functionality

--- Map_Implementation/main.c
+++ Map_Implementation/main.c
@@ -1,5 +1,5 @@
 /* --COPYRIGHT--,BSD
- * Copyright (c) 2017, Texas Instruments Incorporated
+ * Copyright (c) 2016, Texas Instruments Incorporated
  * All rights reserved.
  *
  * Redistribution and use in source and binary forms, with or without
@@ -30,110 +30,118 @@
  * EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
  * --/COPYRIGHT--*/
 
+#include <audio.h>
+#include <display.h>
+#include <flash_memory.h>
+#include <key_press.h>
+#include <keypad.h>
+#include <status_fsm.h>
+#include <string.h>
+#include <hal.h>
+
 #include "driverlib.h"
-#include "key_press.h"
 
-//******************************************************************************
-//!
-//!   Empty Project that includes driverlib
-//!
-//******************************************************************************
-
-#define MAP_LENGTH 9
-
-//// Spanish Symbol Mappings.
-//#define A_RIGHT_ACCENT                  'z'
-//#define E_RIGHT_ACCENT                  'x'
-//#define I_RIGHT_ACCENT                  'c'
-//#define O_RIGHT_ACCENT                  'v'
-//#define U_RIGHT_ACCENT                  'b'
-//#define U_DIAERESIS                     'n'
-//#define N_TENUTO                        'm'
-//#define QUESTION_MARK_INVERTED          ','
-//#define EXCLAMATION_POINT_INVERTED      '.'
-//
-//// French Symbol Mappings.
-//#define A_LEFT_ACCENT                   '1'
-//#define A_CARET                         '2'
-////#define E_RIGHT_ACCENT                '3'
-//#define E_LEFT_ACCENT                   '4'
-//#define E_CARET                         '5'
-//#define E_DIAERESIS                     '6'
-//#define I_DIAERESIS                     '7'
-//#define I_CARET                         '8'
-//#define O_CARET                         '9'
-//#define U_LEFT_ACCENT                   '0'
-//#define U_CARET                         '-'
-////#define U_DIAERESIS                   '='
-//#define C_CEDILLA                       'q'
-//#define Y_DIAERESIS                     'w'
-//#define AE                              'e'
-//#define OE                              'r'
-
-//const uint8_t g_spanish_characters[NUM_SPANISH_CHARACTERS] = {
-//    A_RIGHT_ACCENT,
-//    E_RIGHT_ACCENT,
-//    I_RIGHT_ACCENT,
-//    O_RIGHT_ACCENT,
-//    U_RIGHT_ACCENT,
-//    U_DIAERESIS,
-//    N_TENUTO,
-//    QUESTION_MARK_INVERTED,
-//    EXCLAMATION_POINT_INVERTED};
-//
-//const uint8_t g_french_characters [NUM_FRENCH_CHARACTERS] = {
-//    A_LEFT_ACCENT,
-//    A_CARET,
-//    E_RIGHT_ACCENT,
-//    E_LEFT_ACCENT,
-//    E_CARET,
-//    E_DIAERESIS,
-//    I_DIAERESIS,
-//    I_CARET,
-//    O_CARET,
-//    U_LEFT_ACCENT,
-//    U_CARET,
-//    U_DIAERESIS,
-//    C_CEDILLA,
-//    Y_DIAERESIS,
-//    AE,
-//    OE};
-//
-//typedef enum
-//{
-//    key1,
-//    key2,
-//    key3,
-//    key4,
-//    key5,
-//    key6,
-//    key7,
-//    key8,
-//    key9,
-//    spanish_tab,
-//    french_tab,
-//    no_key
-//} button_t;
+#include "USB_config/descriptors.h"
+#include "USB_API/USB_Common/device.h"
+#include "USB_API/USB_Common/usb.h"                  // USB-specific functions
+#include "USB_API/USB_HID_API/UsbHid.h"
+#include "USB_app/keyboard.h"
 
 
-typedef struct
-{
-    button_t key;
-    uint8_t value;
-} map_t;
+button_t key;
+status_fsm_t status_fsm;
 
-map_t example_map[MAP_LENGTH];// = {{key1, 0}, {key2, 0}};
-button_t i;
+
 void main (void)
 {
-    for (i=key1; i<=key9; i++)
-    {
-        map_t temp_map = {i, g_spanish_characters[i]};
-        example_map[i] = temp_map;
-    }
-    example_map[0].value = 1;
-    example_map[1].value = 2;
-    while (1) {
+    WDT_A_hold(WDT_A_BASE); // Stop watchdog timer
 
+    clock_init(25000000);   // Config clocks. MCLK=SMCLK=FLL=25MHz; ACLK=REFO=32kHz
+    flash_memory_init();
+    audio_init();
+    status_fsm_init(&status_fsm);
+    key_press_init();
+    display_init();
+
+
+    // ------------- Physical Keyboard --------------------------------
+    keypad_init();
+    // ------------- Physical Keyboard --------------------------------
+
+    Keyboard_init();
+    USB_setup(TRUE, TRUE);
+
+    __enable_interrupt();
+
+    while (1)
+    {
+        // ------------- Physical Keyboard --------------------------------
+        key = switch_press_scan();
+        g_key_press_info.b_key_press_detected = TRUE;
+        if ((key == spanish_tab) || (key == french_tab))
+        {
+            g_key_press_info.action = change_page;
+        }
+        else if (g_time_elapsed > 300000) {
+            g_key_press_info.action = move_key;
+        }
+        else
+        {
+            g_key_press_info.action = send_key;
+        }
+        g_key_press_info.pressed_key = key;
+        // ------------- Physical Keyboard --------------------------------
+
+        // VERIFY THAT THE USB DEVICE IS PROPERLY CONNECTED.
+        switch(USB_getConnectionState())
+        {
+            // WWHEN THE USB DEVICE IS PROPERLY CONNECTED, HANDLE INPUTS
+            case ST_ENUM_ACTIVE:
+                // IF THE DEVICE IS IDLE AND A KEY PRESS IS DETECTED, HANDLE IT ACCORDINGLY.
+                if (g_key_press_info.b_key_press_detected && (status_fsm.current_state == idle)) {
+                    // CHANGE THE PAGE IF THE USER SELECTED ANOTHER TAB.
+                    if (g_key_press_info.action == change_page)
+                    {
+                        status_fsm.current_state = change_page;
+                        play_sound(change_page);
+                        change_current_language(g_key_press_info.pressed_key);
+                        update_display();
+                        status_fsm.current_state = idle;
+                    }
+
+                    // TYPE A KEY IF THE USER PRESSED A KEY.
+                    else if (g_key_press_info.action == send_key)
+                    {
+                        status_fsm.current_state = send_key;
+                        play_sound(send_key);
+                        uint8_t selected_character = get_key_from_button(g_key_press_info.pressed_key);
+                        special_key_press(selected_character);
+                        status_fsm.current_state = idle;
+                    }
+
+                    // REARRANGE A KEY IF A USER HELD DOWN A KEY.
+                    else if (g_key_press_info.action == move_key)
+                    {
+                        status_fsm.current_state = move_key;
+                        play_sound(move_key);
+                        move_key_to_front(g_key_press_info.pressed_key);
+                        update_display();
+                        status_fsm.current_state = idle;
+                    }
+                }
+                g_key_press_info.b_key_press_detected = FALSE;
+                break;
+
+            // WHEN THE USB DEVICE IS NOT PROPERLY CONNECTED, DON'T HANDLE INPUTS.
+            case ST_PHYS_DISCONNECTED:
+            case ST_ENUM_SUSPENDED:
+            case ST_PHYS_CONNECTED_NOENUM_SUSP:
+                __bis_SR_register(LPM3_bits + GIE);
+                _NOP();
+                break;
+            case ST_ENUM_IN_PROGRESS:
+            default:;
+        }
     }
 }
+
