80542c2ad058dbd62df99d8c65c2e93d24f1cfca
Christopher Hamilton <christopherarthamilton@gmail.com>
2021-11-30 14:03:30 -0500

    Add flash mem

--- LCD_USB/flash_memory.h
+++ LCD_USB/flash_memory.h
@@ -8,8 +8,10 @@
 #ifndef FLASH_MEMORY_H_
 #define FLASH_MEMORY_H_
 
+#define MEMORY_LOCATION 0x1800
 
 void flash_memory_init(void);
-
+void save_mappings(void);
+void load_mappings(void);
 
 #endif /* FLASH_MEMORY_H_ */
